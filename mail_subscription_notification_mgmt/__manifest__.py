499ba275827835555c9939d2263746c5c2974a7d
Majda EL MARIOULI <majdaelmariouli@gmail.com>
2022-07-05 14:22:01 +0100

    TA#44481 add module mail_subscription_notification_mgmt (#8)
    * TA#44481 add module mail_subscription_notification_mgmt
    
    * fix images path
    
    Co-authored-by: Quentin Lavallée-Bourdeau <31708522+qtiplb@users.noreply.github.com>

---
+++ mail_subscription_notification_mgmt/__manifest__.py
@@ -0,0 +1,16 @@
+# © 2022 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    'name': 'Disable Subscription Notification',
+    'version': '1.0.0',
+    'author': 'Numigi',
+    'maintainer': 'Numigi',
+    'website': "https://bit.ly/numigi-com",
+    'license': 'LGPL-3',
+    'category': 'Other',
+    'summary': 'Manage subscription notification of a model.',
+    'depends': ['mail'],
+    'data': ['views/ir_model_views.xml'],
+    'installable': True,
+}
