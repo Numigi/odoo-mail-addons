a18d35d2a89981577b17004278cc3fc9cce2de47
Lanto Razafindrabe <lanto.razafindrabe@numigi.com>
2023-06-06 08:04:21 +0300

    TA#53374 [ADD] fetchmail_outlook_by_company : adding module base

---
+++ fetchmail_outlook_by_company/README.rst
@@ -0,0 +1,17 @@
+Fetchmail Outlook by company
+============================
+This module offers the possibility to use Outlook configuration per company, for outgoing mail server/inbound Mail Server.
+
+Dependancy
+----------
+The module depends on the module `fetchmail_outlook`.
+
+Usage
+-----
+____....................________
+
+.. image:: static/description/_____________.png
+
+Contributors
+------------
+* Numigi (tm) and all its contributors (https://bit.ly/numigiens)
---
+++ fetchmail_outlook_by_company/__manifest__.py
@@ -0,0 +1,15 @@
+# © 2023 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/AGPL).
+
+{
+    'name': 'Fetchmail Outlook by company',
+    'version': "14.0.1.0.0",
+    'author': 'Numigi',
+    'maintainer': 'Numigi',
+    'website': 'https://www.numigi.com',
+    'license': 'AGPL-3',
+    'category': 'Other',
+    'summary': 'Add the possibility to use Outlook configuration per company.',
+    'depends': ['fetchmail_outlook'],
+    'installable': True,
+}
