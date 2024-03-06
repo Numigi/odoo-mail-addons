92245f98db2bc354341e65562f3684e431269361
Majda EL MARIOULI <majdaelmariouli@gmail.com>
2024-03-06 14:56:24 -0500

    TA#63122 [MIG] Module mail_activity_not_deleted (#19)
    * Port mail_activity_not_deleted to 12.0 (#8)
    
    * Port mail_activity_not_deleted to 12.0
    
    * Fix feedback message on activities (#31)
    
    * TA#34363 port auditlog to v14
    
    Remove super_calendar (will not be supported anymore by Numigi.
    
    * TA#34363 port modules to odoo version 14.0
    
    * Ta#47907 [MIG][14.0] (#107)
    
    * [MIG] base_extended_security: Migration to 14.0
    
    * TA#47907 [MIG][14.0] : auditlog,base_extended_security, test_http_request
    
    * TA#47907 [MIG][14.0] : All modules + 2 others
    
    * TA#47907 [MIG][14.0] : change ir_attachment_name_autocomplete name
    
    * TA#47907 [MIG][14.0] : add ir_attachment_name_autocomplete name in the mainifest
    
    * TA#47907 [MIG][14.0] : fix test setupclass
    
    * TA#47907 [MIG][14.0] : update dockerfile
    
    * TA#47907 [MIG][14.0] : update test setup
    
    * TA#47907 [MIG][14.0] : update test setup
    
    * TA#47907 [MIG][14.0] : update test setup
    
    * TA#48105 [ADD] module auth_oauth_microsoft (#113)
    
    * TA#48502 [IMP] Add pre-commit-config, flake8 and workflow to repo (#132)
    
    * TA#48502 [IMP] Add pre-commit-config, flake8 and workflow to repo
    
    * TA#63122 [FIX] migration mail_activity_not_deleted (#138)
    
    * TA#63122 [MIG] Module mail_activity_not_deleted
    
    ---------
    
    Co-authored-by: David Dufresne <27902736+ddufresne@users.noreply.github.com>
    Co-authored-by: Abdellatif BENZBIRIA <a.benzbiria@magnetomedia.net>

--- .docker_files/main/__manifest__.py
+++ .docker_files/main/__manifest__.py
@@ -2,16 +2,17 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
-    'name': 'Main Module',
-    'version': '1.0.0',
-    'author': 'Numigi',
-    'maintainer': 'Numigi',
-    'website': 'https://www.numigi.com',
-    'license': 'LGPL-3',
-    'category': 'Other',
-    'summary': 'Install all addons required for testing.',
-    'depends': [
-        'fetchmail_outlook_by_company',
+    "name": "Main Module",
+    "version": "1.0.0",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "website": "https://www.numigi.com",
+    "license": "LGPL-3",
+    "category": "Other",
+    "summary": "Install all addons required for testing.",
+    "depends": [
+        "fetchmail_outlook_by_company",
+        "mail_activity_not_deleted",
     ],
-    'installable': True,
+    "installable": True,
 }
