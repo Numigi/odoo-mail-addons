813b177a96608fc71b121369ea78ad05a745b51d
Abdellatif BENZBIRIA <a.benzbiria@magnetomedia.net>
2025-05-02 15:25:00 +0100

    Change copyright text

--- mail_template_archive/models/mail_template.py
+++ mail_template_archive/models/mail_template.py
@@ -1,6 +1,5 @@
-# mail_template_archive/models/mail_template.py
-# © 2021 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# Copyright 2025 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl)
 
 from odoo import models, fields
 
