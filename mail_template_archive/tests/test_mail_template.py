23e0da3d4d4b16611bb8d0c5fe8f7fa0cbf4dc97
Abdellatif BENZBIRIA <a.benzbiria@gmail.com>
2025-05-02 15:22:05 +0100

    Update test_mail_template.py

--- mail_template_archive/tests/test_mail_template.py
+++ mail_template_archive/tests/test_mail_template.py
@@ -1,5 +1,5 @@
-# © 2021 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# Copyright 2025 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl)
 
 from odoo.tests import SavepointCase
 
