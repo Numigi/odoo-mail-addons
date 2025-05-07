28e2b9fa26b6f296bd5b61b4c0226601aadbba1c
Abdellatif Benzbiria <a.benzbiria@gmail.com>
2025-05-07 19:37:47 +0100

    TA#73745 [IMP] mail_portal_company_website

--- mail_portal_company_website/models/mail_thread.py
+++ mail_portal_company_website/models/mail_thread.py
@@ -1,9 +1,7 @@
 # Copyright 2025 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-import re
-from odoo import api, models, tools
-from odoo.tools import config, ustr
+from odoo import models
 
 
 class MailThread(models.AbstractModel):
