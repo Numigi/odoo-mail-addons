5ea0f950998b6fa354c971e7059e946c29599ec7
Abdellatif Benzbiria <odoo-base@numigi.com>
2026-06-08 09:16:29 +0000

    [18.0][MIG] mail_activity_not_deleted

--- mail_activity_not_deleted/__manifest__.py
+++ mail_activity_not_deleted/__manifest__.py
@@ -1,6 +1,9 @@
 # Copyright 2023 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
+# pylint: disable=pointless-statement
+# noqa: B018
+
 {
     "name": "Mail Activity Not Deleted",
     "version": "18.0.1.0.0",
