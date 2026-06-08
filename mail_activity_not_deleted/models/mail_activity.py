8b4acdb3e2a3b00e05317788f720275236be6130
Abdellatif Benzbiria <odoo-base@numigi.com>
2026-06-08 09:57:11 +0000

    [18.0][MIG] mail_activity_not_deleted

--- mail_activity_not_deleted/models/mail_activity.py
+++ mail_activity_not_deleted/models/mail_activity.py
@@ -34,7 +34,7 @@ class MailActivityInactivatedInsteadOfDeleted(models.Model):
             lambda act: act.date_deadline <= fields.Date.today()
         )
         if todo_activities:
-            self.env["bus.bus"]._sendmany(
+            self.env["bus.bus"]._sendone(
                 [
                     (partner, "mail.activity/updated", {"activity_deleted": True})
                     for partner in todo_activities.user_id.partner_id
